177bdeb6e716f228a965305acbc56b76f033cad1
Rostyslav <shev4uk.rostik@gmail.com>
2018-11-02 15:21:29 +0200

    Второе ДЗ
    Написан скрипт для слайдшоу

---
+++ docs/js/slideshow.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	var slideIndex = 0;
+
+	showPhotos();
+
+	function showPhotos(){
+
+		var i;
+		var slides = $('.photo img');
+		console.log(slides);
+		console.log(slides.length); //12
+
+		// Запускаем цикл для скрития фотографий
+		for ( i = 0; i < slides.length; i++ ){
+			console.log(i);
+			$(slides[i]).hide();
+		}
+	
+		slideIndex++;
+		console.log(slideIndex);
+		if ( slideIndex > slides.length ){
+			slideIndex = 1
+		}
+
+		$(slides[slideIndex-1]).show();
+		setTimeout(showPhotos, 200);
+
+	}
+
+});
