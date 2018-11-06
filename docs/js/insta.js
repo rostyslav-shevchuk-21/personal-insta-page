7040ba996b288450ef04181dec7b931a6818b920
Rostyslav <shev4uk.rostik@gmail.com>
2018-11-06 12:46:53 +0200

    Третье ДЗ
    Выводим на страницу фотографии из Instagram!

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){ 
 
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '2312846682',
+        accessToken: '2312846682.1677ed0.04f54dce7f144d1dad3b5d57720b114e',
+        resolution: 'standard_resolution',
+        limit: 9
+    });
+    userFeed.run();
+
+    setTimeout(function(){
+    	$('.instafeed').addClass('shadowed');
+    }, 100);
 
 
 });
--- docs/js/slideshow.js
+++ docs/js/slideshow.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	function showPhotos(){
 
 		var i;
-		var slides = $('.photo img');
+		var slides = $('#instafeed a');
 		console.log(slides);
 		console.log(slides.length); //12
 
@@ -24,7 +24,7 @@ $(document).ready(function(){
 		}
 
 		$(slides[slideIndex-1]).show();
-		setTimeout(showPhotos, 200);
+		setTimeout(showPhotos, 800);
 
 	}
 
